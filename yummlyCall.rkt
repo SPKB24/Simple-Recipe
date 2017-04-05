ac5f0a2dc554317d6bed0eecbdff3ac93d8c010e
Sohit Pal <sohitpal@outlook.com>
2017-04-05 13:48:18 -0400

    Add comments to prior procedures, and put yummly API credentials in another file

--- yummlyCall.rkt
+++ yummlyCall.rkt
@@ -1,26 +1,29 @@
 #lang racket
 
+(require "yummlyAPI.rkt")
+
 (require net/url)
 (require json)
 
-(define yummlyUrl "http://api.yummly.com/v1/api/recipes?_app_id=ea7d9b93&_app_key=8df2515c1801eb355a8a879ce265df92&q=")
+(define yummlyURL (string-append "http://api.yummly.com/v1/api/recipes?_app_id=" appID "&_app_key=" appKey "&q="))
 
+;; add-ingredients will take a list of strings to parse and add to the API call url
+;;   ex. (add-ingredients (list "chicken" "eggs" "cheese"))
 (define (add-ingredients ingredients)
   (if (null? ingredients)
-      yummlyUrl
+      yummlyURL
       (begin
         (if (null? (cdr ingredients))
-            (set! yummlyUrl (string-append yummlyUrl (car ingredients)))
-            (set! yummlyUrl (string-append yummlyUrl (car ingredients) "+")))
+            (set! yummlyURL (string-append yummlyURL (car ingredients)))
+            (set! yummlyURL (string-append yummlyURL (car ingredients) "+")))
         (add-ingredients (cdr ingredients)))))
 
+;; Once all information has been added from the GUI to the yummlyURL, call this procedure
+;; to actually send out the API call.
 (define (getRecipes)
   (define in
     (get-pure-port
-     (string->url yummlyUrl)))
+     (string->url yummlyURL)))
   (define response-string (port->string in))
   (close-input-port in)
   (printf response-string))
-
-(define (parseResponse responseString)
-  (hash-ref (car (hash-ref (string->jsexpr responseString) 'resultSets)) 'rowSet))
